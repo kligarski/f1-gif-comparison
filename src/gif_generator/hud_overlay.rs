23879105c15250009b95a4c55c166e30d0be5c33
Krzysztof Ligarski <kligarski@student.agh.edu.pl>
2024-05-30 23:53:57 +0200

    Driver HUD part 2 - Sector times

--- src/gif_generator/hud_overlay.rs
+++ src/gif_generator/hud_overlay.rs
@@ -9,14 +9,16 @@ use super::{SIDEBAR_WIDTH, TRACK_HEIGHT};
 const DRIVER_STATS_HEIGHT: u32 = 200;
 const PADDING_LR: u32 = 20;
 const PADDING_TB: u32 = 38;
-const PADDING_TB_INNER: u32 = 20;
+const PADDING_TB_INNER: u32 = 10;
 
 const DRIVER_TEAM_MARGIN: i32 = -5;
 const NAME_LAP_SPEED_MARGIN: u32 = 5;
+const SECTOR_TIMES_MARGIN: u32 = 3;
 
 const DRIVER_FONT_SIZE: u32 = 20;
 const TEAM_FONT_SIZE: u32 = 12;
 const LAP_SPEED_FONT_SIZE: u32 = 24;
+const SECTOR_FONT_SIZE: u32 = TEAM_FONT_SIZE;
 
 fn has_finished(driver_data: &CompleteDriverData, current_frame: usize) -> bool {
     current_frame >= driver_data.telemetry.len()
@@ -78,6 +80,51 @@ fn get_time(time: i32, font: &FontRef) -> RgbaImage {
     time_buffer
 }
 
+fn time_to_sector_time_str(time: i32) -> String {
+    let seconds = (time / 1000) % 60;
+    let miliseconds = time % 1000;
+
+    format!("{:0>2}.{:0>3}", seconds, miliseconds)
+}
+
+fn get_sector_time_str(driver_data: &CompleteDriverData, sector: u8, current_frame: usize) -> String {
+    let sector_session_time = match sector {
+        1 => driver_data.lap.sector1_session_time,
+        2 => driver_data.lap.sector2_session_time,
+        3 => driver_data.lap.sector3_session_time,
+        _ => i64::MAX
+    };
+
+    let sector_time = match sector {
+        1 => driver_data.lap.sector1_time,
+        2 => driver_data.lap.sector2_time,
+        3 => driver_data.lap.sector3_time,
+        _ => i32::MAX
+    };
+
+    if current_frame >= driver_data.telemetry.len() || driver_data.telemetry[current_frame].session_time >= sector_session_time {
+        format!("Sector {}: {}", sector, time_to_sector_time_str(sector_time))
+    } else {
+        String::from("")
+    }
+}
+
+fn get_sector_times(driver_data: &CompleteDriverData, current_frame: usize, font: &FontRef) -> RgbaImage {
+    let font_scale = font.pt_to_px_scale(SECTOR_FONT_SIZE as f32).unwrap_or(PxScale::from(SECTOR_FONT_SIZE as f32));
+    let height = font.as_scaled(font_scale).height().ceil() as u32;
+
+    let mut sector_times_buffer = RgbaImage::from_pixel(SIDEBAR_WIDTH, 3 * height, Rgba::from([255, 255, 255, 0]));
+
+    for i in 0..3 {
+        let mut sector_time_buffer = RgbaImage::from_pixel(SIDEBAR_WIDTH, height, Rgba::from([255, 255, 255, 0]));
+        let sector_time_str = get_sector_time_str(&driver_data, i + 1, current_frame);
+        draw_text_mut(&mut sector_time_buffer, Rgba::white(), PADDING_LR as i32, 0, font_scale, font, &sector_time_str);
+        overlay(&mut sector_times_buffer, &sector_time_buffer, 0, i as i64 * height as i64);
+    }
+
+    sector_times_buffer
+}
+
 fn get_driver_stats(driver_data: &CompleteDriverData, current_frame: usize, regular_font: &FontRef, bold_font: &FontRef) -> RgbaImage {
     let mut stats = RgbaImage::from_pixel(SIDEBAR_WIDTH, DRIVER_STATS_HEIGHT, Rgba::from([255, 255, 255, 0]));
 
@@ -89,10 +136,11 @@ fn get_driver_stats(driver_data: &CompleteDriverData, current_frame: usize, regu
     } else {
         get_speed(driver_data, current_frame, bold_font)
     };
-    // let sector_times = get_sector_times();
+    let sector_times = get_sector_times(driver_data, current_frame, regular_font);
 
     overlay(&mut stats, &driver_name_buffer, 0, (PADDING_TB_INNER) as i64);
     overlay(&mut stats, &time_or_speed, 0, (PADDING_TB_INNER + driver_name_buffer.height() + NAME_LAP_SPEED_MARGIN) as i64);
+    overlay(&mut stats, &sector_times, 0, (DRIVER_STATS_HEIGHT - sector_times.height() - SECTOR_TIMES_MARGIN) as i64);
 
     stats
 }
